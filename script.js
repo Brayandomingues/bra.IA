bf75f4c4e97eeee95e10ea4ede6a73b62eeb0a7e
Brayandomingues <brayan.zanon.silva@escola.pr.gov.br>
2025-09-10 20:57:48 -0300

    ok

---
+++ script.js
@@ -0,0 +1,21 @@
+const caixaPrincipal=document.querySelector(".caixa-principal");
+const caixaPerguntas=document.querySelector(".caixa-perguntas");
+const caixaAlternativas=document.querySelector(".caixa-alternativas");
+const caixaResultado=document.querySelector(".caixa-resultado");
+const caixaResultado=document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado:"Assim que saiu do colégio você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e aúdios. Qual é seu primeiro pensamento?",
+        alternativas:[
+            {
+         texto: "Iso é assustador!",
+         afirmacao:"No início, eu ficaria com medo do que essa tecnologia pode fazer."
+
+            },
+            {
+                
+            }
+        ]
+    }
+]
